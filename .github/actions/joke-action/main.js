7cccbd4680362ba8530496392624a7b131955de4
Alan Jose <alanjose10@gmail.com>
2024-05-28 00:31:16 +0100

    creating joke.js and main.js

---
+++ .github/actions/joke-action/main.js
@@ -0,0 +1,12 @@
+
+
+const getJoke = require('./joke');
+const core = require('@actions/core');
+
+const run = async () => {
+    const joke = await getJoke();
+    console.log(joke);
+    core.setOutput("joke", joke);
+}
+
+run();
